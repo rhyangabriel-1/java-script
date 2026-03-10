a800c265a0e680b161cc4c25e9c72f1452c4ecfb
rhyangabriel <rhyan.belem@aluno.senai.br>
2026-03-10 14:38:45 -0300

    AtividadesDom

--- dom/332026/atividade.js
+++ dom/332026/atividade.js
@@ -12,3 +12,49 @@ vermelho.addEventListener("click", function() {
 });
 
 //Atividade 2- mostrar e esconder mensagem 
+
+let mostrar = document.getElementById("mostrar");
+let mensagem = document.getElementById("mensagem")
+
+mostrar.addEventListener("click", function() {
+    mensagem.classList.toggle('oculto')
+});
+
+//Atividade 3- Contador de caracteres
+let input = document.getElementById("inputTexto");
+let contador = document.getElementById("contador");
+
+input.addEventListener("input", function() {    let caracteres = input.value.length;
+    contador.textContent = caracteres + " caracteres";
+});
+
+//Atividade 4- Criar botão contador
+let contadorBotao = document.querySelector("#contadorBotao");
+let valorContador = document.querySelector("#valorContador");
+let contagem = 0;
+
+contadorBotao.addEventListener("click", function() {
+    contagem++;
+    valorContador.textContent = contagem;
+});
+
+//Atividade 5 e 6- Criar elementos dinamicamente
+let novoElemento = document.getElementById("novoElemento");
+let criarElemento = document.getElementById("criarElemento");
+let lista = document.getElementById("lista");
+
+criarElemento.addEventListener("click", function() {
+    let novoItem = document.createElement("li");
+    novoItem.textContent = novoElemento.value + " - excluir ";
+
+    novoItem.addEventListener("click", function() {
+
+        comfirmacao = confirm("Deseja excluir este item?");
+        if (comfirmacao) {
+        lista.removeChild(novoItem);
+    }
+
+});
+lista.appendChild(novoItem);
+novoElemento.value = "";
+});
